8a9bccd0dc86ac033b04279a3570342f388456ce
AlejandroCabrera19 <alejandrocabrera535@yahoo.com>
2021-06-19 16:46:43 -0400

    Update 6/19/2021

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ Purpose: The purpose is to set up a database and get a stream of JSON in
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from views import views
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f52e426cb2bc827ee460520ef2e02ee6'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-#HINT: sqlite:///db.sqlite3
+app.config["SECRET_KEY"] = "f52e426cb2bc827ee460520ef2e02ee6"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+# HINT: sqlite:///db.sqlite3
 db = SQLAlchemy(app)
 from customers.models import Customer
 from views import views
+
 app.register_blueprint(views)
-
-
