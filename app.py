962b870a871389455119a6592b6a4d66a5f24ff7
AlejandroCabrera19 <alejandrocabrera535@yahoo.com>
2021-06-25 16:03:11 -0400

    Commit at 4:03 PM on 6/25/2021

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ Purpose: The purpose is to set up a database and get a stream of JSON in
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from views import views
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "f52e426cb2bc827ee460520ef2e02ee6"
