430ef4a379d46a640f27d115c420aaf0b4b92afa
AlejandroCabrera19 <72628907+AlejandroCabrera19@users.noreply.github.com>
2021-07-21 18:07:38 -0400

    Feature/add crud to customer blueprint (#2)
    * Update 6/19/2021
    
    * Commit at 4:03 PM on 6/25/2021
    
    * Commit at 7:46 PM on 6/27/2021
    
    * Commit at 10:24 PM on 6/28/2021
    
    * Commit at 6:35 PM on 6/30/2021
    
    * Small fix
    
    * Added Response Numbers to return statements, rearranged the list and detail routes, and moved the url prefix to the views file
    
    * Removed the paths from routes and other clean up
    
    * Reformatted the list function to return a list JSON objects
    
    * Reformatted the retrieve method
    
    * Git Housekeeping

--- customers/views.py
+++ customers/views.py
@@ -1,15 +1,17 @@
-import customers
+import customers, json
 from flask import Blueprint, flash, render_template, request
 from app import db
 from customers.models import Customer
 from sqlalchemy import exc
-from json import json
+import json
 
-views = Blueprint("views", __name__, template_folder="templates")
+views = Blueprint(
+    "views", __name__, template_folder="templates", url_prefix="/customers"
+)
 
 
-@views.route("/json-post", methods=["POST"])
-def json_post():
+@views.route("/", methods=["POST"])
+def create():
     try:
         # The block of code that is supposed to execute if a new customer is to be placed in the database
         request_data = request.get_json()
@@ -35,11 +37,106 @@ def json_post():
             "email": f"{email}",
             "dob": f"{dob}",
             "date_created": f"{newCustomer.date_created}",
-        }
+        }, 200
     except exc.IntegrityError:
         # Error Code 409, for resource already exists / duplicate resource / Conflict
         # Error that occurs when a customer already has a place in the database
-        return {"Message": "Error 409, Customer already exists"}
+        # Should occur when a customer enters a unique field such as email and username more than once
+        return {"Message": "ERROR, Code: 409, Customer already exists"}, 409
     except KeyError:
         # Error that occurs when a user leaves fields empty
-        return {"Message": "ERROR, Code: 422, left required fields empty"}
+        return {"Message": "ERROR, Code: 422, left required fields empty"}, 422
+
+
+@views.route("/", defaults={"customer_id": None}, methods=["GET"])
+@views.route("/<int:customer_id>", methods=["GET"])
+def retrieve(customer_id):
+    try:
+        if customer_id is None:
+            customer_list = []
+            for selected_customer in Customer.query.all():
+                customer_list.append(
+                    {
+                        "id": f"{selected_customer.id}",
+                        "username": f"{selected_customer.username}",
+                        "first_name": f"{selected_customer.first_name}",
+                        "last_name": f"{selected_customer.last_name}",
+                        "email": f"{selected_customer.email}",
+                        "dob": f"{selected_customer.dob}",
+                        "date_created": f"{selected_customer.date_created}",
+                    }
+                )
+            return json.dumps(customer_list), 200
+        else:
+            selected_customer = Customer.query.filter_by(id=customer_id).first()
+            returning_data = {
+                "id": f"{selected_customer.id}",
+                "username": f"{selected_customer.username}",
+                "first_name": f"{selected_customer.first_name}",
+                "last_name": f"{selected_customer.last_name}",
+                "email": f"{selected_customer.email}",
+                "dob": f"{selected_customer.dob}",
+                "date_created": f"{selected_customer.date_created}",
+            }
+            return json.dumps(returning_data), 200
+    except Exception as e:
+        print(e)
+        return {"Message": "ERROR, invalid id number entered"}, 404
+
+
+@views.route("/<int:customer_id>", methods=["PATCH", "PUT"])
+def update(customer_id):
+    try:
+        request_data = request.get_json()
+        updated_customer = Customer.query.filter_by(id=customer_id).first()
+        if request.method == "PUT":
+            updated_customer.username = request_data["username"]
+            updated_customer.first_name = request_data["first_name"]
+            updated_customer.last_name = request_data["last_name"]
+            updated_customer.email = request_data["email"]
+            updated_customer.dob = request_data["dob"]
+            db.session.commit()
+            return {
+                "id": f"{updated_customer.id}",
+                "username": f"{updated_customer.username}",
+                "first_name": f"{updated_customer.first_name}",
+                "last_name": f"{updated_customer.last_name}",
+                "email": f"{updated_customer.email}",
+                "dob": f"{updated_customer.dob}",
+                "date_created": f"{updated_customer.date_created}",
+            }, 200
+        elif request.method == "PATCH":
+            if "username" in request_data:
+                updated_customer.username = request_data["username"]
+            if "first_name" in request_data:
+                updated_customer.first_name = request_data["first_name"]
+            if "last_name" in request_data:
+                updated_customer.last_name = request_data["last_name"]
+            if "email" in request_data:
+                updated_customer.email = request_data["email"]
+            if "dob" in request_data:
+                updated_customer.dob = request_data["dob"]
+            return {
+                "id": f"{updated_customer.id}",
+                "username": f"{updated_customer.username}",
+                "first_name": f"{updated_customer.first_name}",
+                "last_name": f"{updated_customer.last_name}",
+                "email": f"{updated_customer.email}",
+                "dob": f"{updated_customer.dob}",
+                "date_created": f"{updated_customer.date_created}",
+            }, 200
+    except KeyError:
+        return {"Message": "ERROR, did not fill all required fields"}, 422
+    except Exception:
+        return {"Message": "ERROR, invalid id number entered"}, 404
+
+
+@views.route("/<int:customer_id>", methods=["DELETE"])
+def delete(customer_id):
+    try:
+        removed_customer = Customer.query.filter_by(id=customer_id).first()
+        db.session.delete(removed_customer)
+        db.session.commit()
+        return "", 204
+    except:
+        return {"Message": "ERROR, invalid id number entered"}, 404
