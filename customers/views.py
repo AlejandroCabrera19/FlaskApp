02e6a0c4d2e6efc4d579463f843d4033ddeac810
AlejandroCabrera19 <alejandrocabrera535@yahoo.com>
2021-07-26 10:16:51 -0400

    Added in the BaseModelMixin as well as Flask Marshmallow and a Customer Schema, may need help with Validation Errors

--- customers/views.py
+++ customers/views.py
@@ -1,11 +1,12 @@
-import customers, json
-from flask import Blueprint, flash, render_template, request
+import json
+from flask import Blueprint, jsonify, request
 from app import db
 from customers.models import Customer
 from sqlalchemy import exc
-import json
+from customers.schemas import CustomerSchema
 
 views = Blueprint("views", __name__, url_prefix="/customers")
+customerSchema = CustomerSchema()
 
 
 @views.route("/", methods=["POST"])
@@ -25,17 +26,8 @@ def create():
             email=email,
             dob=dob,
         )
-        db.session.add(newCustomer)
-        db.session.commit()
-        return {
-            "id": f"{newCustomer.id}",
-            "username": f"{userName}",
-            "first_name": f"{firstName}",
-            "last_name": f"{lastName}",
-            "email": f"{email}",
-            "dob": f"{dob}",
-            "date_created": f"{newCustomer.date_created}",
-        }, 200
+        newCustomer.save()
+        return (customerSchema.jsonify(newCustomer)), 200
     except exc.IntegrityError:
         # Error Code 409, for resource already exists / duplicate resource / Conflict
         # Error that occurs when a customer already has a place in the database
@@ -53,30 +45,11 @@ def retrieve(customer_id):
         if customer_id is None:
             customer_list = []
             for selected_customer in Customer.query.all():
-                customer_list.append(
-                    {
-                        "id": f"{selected_customer.id}",
-                        "username": f"{selected_customer.username}",
-                        "first_name": f"{selected_customer.first_name}",
-                        "last_name": f"{selected_customer.last_name}",
-                        "email": f"{selected_customer.email}",
-                        "dob": f"{selected_customer.dob}",
-                        "date_created": f"{selected_customer.date_created}",
-                    }
-                )
+                customer_list.append(selected_customer.to_dict())
             return json.dumps(customer_list), 200
         else:
             selected_customer = Customer.query.filter_by(id=customer_id).first()
-            returning_data = {
-                "id": f"{selected_customer.id}",
-                "username": f"{selected_customer.username}",
-                "first_name": f"{selected_customer.first_name}",
-                "last_name": f"{selected_customer.last_name}",
-                "email": f"{selected_customer.email}",
-                "dob": f"{selected_customer.dob}",
-                "date_created": f"{selected_customer.date_created}",
-            }
-            return json.dumps(returning_data), 200
+            return json.dumps(selected_customer.to_dict()), 200
     except Exception as e:
         print(e)
         return {"Message": "ERROR, invalid id number entered"}, 404
@@ -93,16 +66,8 @@ def update(customer_id):
             updated_customer.last_name = request_data["last_name"]
             updated_customer.email = request_data["email"]
             updated_customer.dob = request_data["dob"]
-            db.session.commit()
-            return {
-                "id": f"{updated_customer.id}",
-                "username": f"{updated_customer.username}",
-                "first_name": f"{updated_customer.first_name}",
-                "last_name": f"{updated_customer.last_name}",
-                "email": f"{updated_customer.email}",
-                "dob": f"{updated_customer.dob}",
-                "date_created": f"{updated_customer.date_created}",
-            }, 200
+            updated_customer.save()
+            return (customerSchema.jsonify(updated_customer)), 200
         elif request.method == "PATCH":
             if "username" in request_data:
                 updated_customer.username = request_data["username"]
@@ -114,15 +79,8 @@ def update(customer_id):
                 updated_customer.email = request_data["email"]
             if "dob" in request_data:
                 updated_customer.dob = request_data["dob"]
-            return {
-                "id": f"{updated_customer.id}",
-                "username": f"{updated_customer.username}",
-                "first_name": f"{updated_customer.first_name}",
-                "last_name": f"{updated_customer.last_name}",
-                "email": f"{updated_customer.email}",
-                "dob": f"{updated_customer.dob}",
-                "date_created": f"{updated_customer.date_created}",
-            }, 200
+            updated_customer.save()
+            return (customerSchema.jsonify(updated_customer)), 200
     except KeyError:
         return {"Message": "ERROR, did not fill all required fields"}, 422
     except Exception:
@@ -133,8 +91,7 @@ def update(customer_id):
 def delete(customer_id):
     try:
         removed_customer = Customer.query.filter_by(id=customer_id).first()
-        db.session.delete(removed_customer)
-        db.session.commit()
+        removed_customer.delete()
         return "", 204
     except:
         return {"Message": "ERROR, invalid id number entered"}, 404
